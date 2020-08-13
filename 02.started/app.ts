e1be1f5184cde946815541d0468b53c6b4fe3080
DalatCoder <hieuntctk42@gmail.com>
2020-08-13 17:50:40 +0700

    02.Object

--- 02.started/app.ts
+++ 02.started/app.ts
@@ -4,6 +4,17 @@ function add(n1: number, n2: number): number {
 
 const number1: number = 5;
 const number2: number = 10;
+const printResult: boolean = true;
 
 const result: number = add(number1, number2);
-console.log(result);
+
+if (printResult) {
+  console.log(result);
+}
+
+const person: { name: string; age: number } = {
+  name: 'Hieu',
+  age: 20,
+};
+
+console.log(person);
