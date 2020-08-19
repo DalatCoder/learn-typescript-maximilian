f11bcbf40a241adb7ab6d236849d009844592a1a
DalatCoder <hieuntctk42@gmail.com>
2020-08-19 20:21:33 +0700

    05.Extract ListItem

--- 06.first-project/build/app.js
+++ 06.first-project/build/app.js
@@ -127,6 +127,24 @@ var Component = /** @class */ (function () {
     };
     return Component;
 }());
+// ProjectItem class
+var ProjectItem = /** @class */ (function (_super) {
+    __extends(ProjectItem, _super);
+    function ProjectItem(hostId, project) {
+        var _this = _super.call(this, 'single-project', hostId, false, project.id) || this;
+        _this.project = project;
+        _this.configure();
+        _this.renderContent();
+        return _this;
+    }
+    ProjectItem.prototype.configure = function () { };
+    ProjectItem.prototype.renderContent = function () {
+        this.element.querySelector('h2').textContent = this.project.title;
+        this.element.querySelector('h3').textContent = this.project.people.toString();
+        this.element.querySelector('p').textContent = this.project.description;
+    };
+    return ProjectItem;
+}(Component));
 // ProjectList class
 var ProjectList = /** @class */ (function (_super) {
     __extends(ProjectList, _super);
@@ -162,9 +180,7 @@ var ProjectList = /** @class */ (function (_super) {
         listEl.innerHTML = '';
         for (var _i = 0, _a = this.assignedProjects; _i < _a.length; _i++) {
             var project = _a[_i];
-            var listItem = document.createElement('li');
-            listItem.textContent = project.title;
-            listEl.appendChild(listItem);
+            new ProjectItem(this.element.querySelector('ul').id, project);
         }
     };
     return ProjectList;
