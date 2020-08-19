76b574a598e148b2ba7027f402c65ed3acd6ecc3
DalatCoder <hieuntctk42@gmail.com>
2020-08-19 07:48:21 +0700

    04.Generic class

--- 04.generic-types/app.ts
+++ 04.generic-types/app.ts
@@ -26,3 +26,31 @@ function extractAndConvert<T extends Object, U extends keyof T>(
 
 // extractAndConvert({}, 'name');
 extractAndConvert({ name: 'Hieu' }, 'name');
+
+class DataStorage<T extends number | string | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('ABC');
+// textStorage.addItem(10);
+textStorage.addItem('abc');
+textStorage.removeItem('abc');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// Error when remove an object
